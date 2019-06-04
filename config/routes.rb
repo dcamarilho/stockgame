c7d88008ff312260731d6ede2f1c5c2b83dc1b6e
Saile Marinho saile-ribeiro@hotmail.com <saile-ribeiro@hotmail.com>
2019-06-04 14:30:38 -0300

    Colors and fonts update

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'stocks/show'
-  get 'stocks/edit'
-  get 'stocks/update'
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :stocks, except: [:destroy] do
     resources :orders, only: [:new, :create]
@@ -15,5 +11,6 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update, :destroy, :show]
 
   get 'dashboard', to: 'user#dashboard'
+
 
 end
