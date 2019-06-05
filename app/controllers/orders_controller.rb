b8de2f8828e49638566bf57dcd76fb45f10d77b3
Kaynan Silva <kaynan_alves@hotmail.com>
2019-06-05 17:24:25 -0300

    Fix order validation logics

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,8 +11,12 @@ class OrdersController < ApplicationController
     @order.price = @stock.price
 
     if @order.save
-      redirect_to dashboard_path, notice: 'Order was successfully created.'
+      flash[:notice] = "Congratulations! #{@order.quantity.abs} #{@order.stock.name} #{@order.quantity.negative? ? 'removed from' : 'added to' } your wallet."
+
+      redirect_to dashboard_wallet_path
     else
+      # Caso ela nao seja salva, retorna para a pagina anterior com uma mensagem de erro
+      flash[:notice] = 'Something wrong happened.'
       render 'stocks/show'
     end
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -6,9 +6,14 @@ class Order < ApplicationRecord
   validates :quantity, presence: true
   validates :amount, presence: true
   validate :check_user_credit
+  validate :check_user_wallet_stock_quantity
 
   before_validation :calcule_amount
   after_save :update_credit
+  after_save :update_user_wallet
+
+
+  private
 
   def calcule_amount
     self.amount = self.price * self.quantity
@@ -19,7 +24,22 @@ class Order < ApplicationRecord
     self.user.save
   end
 
+  def update_user_wallet
+    user = self.user
+    if user.wallet[self.stock_id.to_s].present?
+      user.wallet[self.stock_id.to_s] += self.quantity
+    else
+      user.wallet[self.stock_id.to_s] = self.quantity
+    end
+    user.save
+  end
+
   def check_user_credit
     errors.add(:user_insuficient_credit, 'insuficient credits') if self.user.credits < self.amount
+  end
+
+  def check_user_wallet_stock_quantity
+    wallet_quantity = self.user.wallet[self.stock_id.to_s] || 0
+    errors.add(:insuficient_stock_amount, 'insuficient stock amount') if wallet_quantity + self.quantity < 0
   end
 end
