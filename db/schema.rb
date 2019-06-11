4f99b51c5a3f97f88591dd8c429a070bcbc79b60
Kaynan Silva <kaynan_alves@hotmail.com>
2019-06-11 13:27:12 -0300

    Correct users index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_10_174347) do
+ActiveRecord::Schema.define(version: 2019_06_10_205741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2019_06_10_174347) do
     t.float "credits", default: 10000.0
     t.string "photo"
     t.jsonb "wallet", default: {}
+    t.integer "experience", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
